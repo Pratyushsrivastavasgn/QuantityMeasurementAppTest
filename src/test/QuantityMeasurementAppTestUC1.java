74ae23be73f091b7051616902439179571443204
Pratyushsrivastavasgn <pratyushsrivastava1sgn>
2026-04-24 14:35:08 +0530

    UC-1

---
+++ src/test/QuantityMeasurementAppTestUC1.java
@@ -0,0 +1,34 @@
+package test;
+import main.QuantityMeasurementAppMainUC1;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class QuantityMeasurementAppTestUC1 {
+    @Test
+    void testEquality_SameValue() {
+        QuantityMeasurementAppMainUC1.Feet a =
+                new QuantityMeasurementAppMainUC1.Feet(1.0);
+        QuantityMeasurementAppMainUC1.Feet b =
+                new QuantityMeasurementAppMainUC1.Feet(1.0);
+        assertTrue(a.equals(b));
+    }
+    @Test
+    void testEquality_DifferentValue() {
+        QuantityMeasurementAppMainUC1.Feet a =
+                new QuantityMeasurementAppMainUC1.Feet(1.0);
+        QuantityMeasurementAppMainUC1.Feet b =
+                new QuantityMeasurementAppMainUC1.Feet(2.0);
+        assertFalse(a.equals(b));
+    }
+    @Test
+    void testEquality_NullComparison() {
+        QuantityMeasurementAppMainUC1.Feet a =
+                new QuantityMeasurementAppMainUC1.Feet(1.0);
+        assertFalse(a.equals(null));
+    }
+    @Test
+    void testEquality_SameReference() {
+        QuantityMeasurementAppMainUC1.Feet a =
+                new QuantityMeasurementAppMainUC1.Feet(1.0);
+        assertTrue(a.equals(a));
+    }
+}
